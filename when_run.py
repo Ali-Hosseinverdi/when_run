f41543ec5cd51531b348951da6c59cefb53edecf
Ali-Hosseinverdi <alihv5000@gmail.com>
2021-03-02 16:01:49 +0330

    fix bug and add select day option in when_run

--- when_run.py
+++ when_run.py
@@ -1,17 +1,22 @@
-import keyboard
 from time import sleep
 from time import time, ctime
+import keyboard
 
-def whenrun(hour,minute):
+
+def whenrun(day,hour,minute):
     time_3 = ['', '', '']
-    while str(hour) != time_3[0] or str(minute) != time_3[1]:
+    while str(hour) != time_3[0] or str(minute) != time_3[1] or str(day) != w_day:
         time_now = ctime(time())
         time_now_clock = time_now.split(' ')
-        time_2 = time_now_clock[3]
+        w_day = time_now_clock[3]
+        time_2 = time_now_clock[4]
         time_3 = time_2.split(':')
         sleep(1)
 
+day = input("when day of month to do : ")
 hour = input("when hour to do : ")
 minute = input("when minute to do : ")
 
-whenrun(hour,minute)
+whenrun(day,hour,minute)
+
+#program code ...
