81c10bb1c3dfcec3fd138d82231580309c3856c5
Ali-Hosseinverdi <alihv5000@gmail.com>
2021-03-02 13:53:21 +0330

    typo fix in when_run.py

--- when_run.py
+++ when_run.py
@@ -1,1 +1,17 @@
+import keyboard
+from time import sleep
+from time import time, ctime
 
+def whenrun(hour,minute):
+    time_3 = ['', '', '']
+    while str(hour) != time_3[0] or str(minute) != time_3[1]:
+        time_now = ctime(time())
+        time_now_clock = time_now.split(' ')
+        time_2 = time_now_clock[3]
+        time_3 = time_2.split(':')
+        sleep(1)
+
+hour = input("when hour to press : ")
+minute = input("when minute to press : ")
+
+whenrun(hour,minute)
